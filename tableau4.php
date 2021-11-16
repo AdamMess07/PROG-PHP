623c00ea43cb8c2b45187308f0cabd59996cce3d
AdamMess07 <adam.messaoudi@esme.fr>
2021-11-16 11:15:24 +0100

    Third commit

---
+++ tableau4.php
@@ -0,0 +1,15 @@
+<?php
+
+$marque = array("volvo","BMW","Toyota");
+
+for ($i=0; $i<count($marque); $i=++;)
+{
+	echo "Achat de : " . $marque[$i] . "<br/>";
+}
+
+         $marque = array("volvo","BMW","Toyota", "Peugeot","Renault",);
+         $i=0;
+         foreach ($elem as $marque) {
+               echo "Vente de : " . $elem . "<br/>";
+} ?>
+
